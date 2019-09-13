0dd3c5b13c8c94e74c2551916ac398b5f2974c81
Sam <super8989@gmail.com>
2019-09-12 20:37:52 -0400

    setting up for word scramble

---
+++ script.js
@@ -0,0 +1,7 @@
+const message = document.querySelector(".message");
+const guess = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+    console.log('click');
+})

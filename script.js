0c03be40e0a98b357b389871edd5312002b8c139
Sam <super8989@gmail.com>
2019-09-12 21:52:16 -0400

    random order array

--- script.js
+++ script.js
@@ -19,10 +19,23 @@ function createWord() {
     let randomIndex = Math.floor(Math.random() * myArray.length);
     let tempWord = myArray[randomIndex];
 
-    console.log(tempWord.split(''));
+    console.log(tempWord);
+
+    let rand = randomArray(tempWord.split(''));
+    console.log(rand.join(''));
 
 }
 
 function randomArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let temp = arr[i];
+        let j = Math.floor(Math.random() * (i + 1));
+        console.log(temp);
+        console.log('i: ' + i); 
+        console.log('j: ' + j); 
 
+        arr[i] = arr[j]; 
+        arr[j] = temp; 
+    }
+    return arr;
 }
